cd166daa52ffaede5d31b43f52f4407bda78c92b
0-l <0th@protonmail.com>
2018-04-30 12:05:19 -0300

    minor fix

--- js/search.js
+++ js/search.js
@@ -12,35 +12,37 @@ $.qS('#search .close').onclick = () =>
   search.classList.remove('active');
 
 document.onkeypress = (e) => {
-  if (e.key == 's')
-    search.classList.add('active');
+  if (document.activeElement !== $.qS('.addTodo input') && document.activeElement !== $.qS('.weather-config input')) {
+    if (e.key == 's')
+      search.classList.add('active');
 
-  input.focus();
-  input.scrollIntoView();
+    input.focus();
+    input.scrollIntoView();
 
-  $.qS('#search .close').onclick = () =>
-    search.classList.remove('active');
-
-  search.onkeyup = (e) => {
-    let args   = e.target.value.split(' '),
-        prefix = args[0],
-        engine = engines['g'][0], // default engine
-        str    = 0;
-
-    $.qA('.search-engines li p').forEach((eng) => {
-      let current = eng.parentNode;
-
-      (prefix == eng.innerHTML)
-        ? current.classList.add('active')
-        : current.classList.remove('active');
-    });
-
-    if (e.key == 'Enter') {
-      if (prefix.indexOf('!') == 0)
-        (engine = engines[prefix.substr(1)][0], str = 3);
-
-      window.location = engine + args.join(' ').substr(str).toString().replace(/\s+/m, '%20');
-    } else if (e.keyCode == 27)
+    $.qS('#search .close').onclick = () =>
       search.classList.remove('active');
-  };
+
+    search.onkeyup = (e) => {
+      let args   = e.target.value.split(' '),
+          prefix = args[0],
+          engine = engines['g'][0], // default engine
+          str    = 0;
+
+      $.qA('.search-engines li p').forEach((eng) => {
+        let current = eng.parentNode;
+
+        (prefix == eng.innerHTML)
+          ? current.classList.add('active')
+          : current.classList.remove('active');
+      });
+
+      if (e.key == 'Enter') {
+        if (prefix.indexOf('!') == 0)
+          (engine = engines[prefix.substr(1)][0], str = 3);
+
+        window.location = engine + args.join(' ').substr(str).toString().replace(/\s+/m, '%20');
+      } else if (e.keyCode == 27)
+        search.classList.remove('active');
+    };
+  }
 };
